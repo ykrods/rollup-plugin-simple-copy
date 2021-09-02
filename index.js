c9edba90192cf67c96be38e5e5f3c4b69366e6fe
ykrods <890082+ykrods@users.noreply.github.com>
2021-09-02 12:33:32 +0900

    Fix filter option position

--- index.js
+++ index.js
@@ -17,14 +17,18 @@ async function* walk(dir) {
 }
 
 function copy(options = { targets: [], filter: null }) {
-  const { targets, filter } = options;
+  const { targets } = options;
 
   return {
     name: 'copy',
     async generateBundle() {
       for (let target of targets) {
+        const stat = await fs.promises.stat(target.src);
+        if (!stat.isDirectory()) {
+          throw new Error(`${target.src} is not a directory`);
+        }
         for await (const src of await walk(target.src)) {
-          if (filter && !filter(src)) {
+          if (target.filter && !target.filter(src)) {
             continue;
           }
           const dest = path.join(target.dest, path.relative(target.src, src));
